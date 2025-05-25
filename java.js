8baa58b9f23d02c3baf72c8c0bfbbee81eb48747
hrt-FI-H09 <haruto.univ@gmail.com>
2025-05-25 12:06:53 +0900

    Add files via upload

---
+++ java.js
@@ -0,0 +1,41 @@
+      var gamelink = [
+        "https://genshin.hoyoverse.com/ja/home",
+        "https://hsr.hoyoverse.com/ja-jp/home",
+        "https://pad.gungho.jp/",
+        "https://www.kirby.jp/",
+        "https://www.nintendo.com/jp/index.html",
+        "https://www.youkai-watch.jp/"
+      ];
+      function random_jump_game() {
+        var i = Math.floor(Math.random() * gamelink.length);
+        window.open(gamelink[i],'_blank');
+      }
+
+      var musiclink =[
+        "https://www.youtube.com/@KenshiYonezu",
+        "https://www.youtube.com/@officialhigedandism",
+        "https://www.youtube.com/@raisan_official",
+        "https://www.youtube.com/@MrsGREENAPPLE_Official"
+      ];
+      function random_jump_music() {
+        var i = Math.floor(Math.random() * musiclink.length);
+         window.open(musiclink[i],'_blank');
+      }
+
+      var animelink =[
+        "https://kimetsu.com/",
+        "https://heroaca.com/",
+        "https://www.jigokuraku.com/",
+        "https://www.ansatsu-anime.com/"
+      ];
+      function random_jump_anime() {
+        var i = Math.floor(Math.random() * animelink.length);
+         window.open(animelink[i],'_blank');
+      }
+
+
+
+
+
+
+	
